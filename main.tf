c9120b2a32279d0c81cd09581a9f3ff40508650f
Tony Pulickal <ttp@hashicorp.com>
2021-06-29 15:46:28 -0400

    update tfc org and workspace name

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "remote" {
-    organization = "hashicorp-learn"
+    organization = "tonyp-corp"
 
     workspaces {
-      name = "learn-terraform-pipelines-vault"
+      name = "terraform-pipelines-vault"
     }
   }
   required_providers {
